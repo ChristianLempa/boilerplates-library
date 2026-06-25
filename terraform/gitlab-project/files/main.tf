56cdc345b9a61c48a3f3c44f71098180c0df2020
xcad <christian.lempa@clcreative.de>
2026-06-25 14:17:13 +0200

    feat(terraform): support gitlab project namespace lookup

--- terraform/gitlab-project/files/main.tf
+++ terraform/gitlab-project/files/main.tf
@@ -1,3 +1,15 @@
+<%- if namespace_group_full_path and not namespace_id %>
+data "gitlab_group" "project_namespace" {
+  full_path = "<< namespace_group_full_path >>"
+}
+
+<%- endif %>
+<%- if namespace_user_username and not namespace_id and not namespace_group_full_path %>
+data "gitlab_user" "project_namespace" {
+  username = "<< namespace_user_username >>"
+}
+
+<%- endif %>
 resource "gitlab_project" "<< resource_name >>" {
   name = "<< project_name >>"
 <%- if path %>
@@ -5,6 +17,10 @@ resource "gitlab_project" "<< resource_name >>" {
 <%- endif %>
 <%- if namespace_id %>
   namespace_id = << namespace_id >>
+<%- elif namespace_group_full_path %>
+  namespace_id = data.gitlab_group.project_namespace.id
+<%- elif namespace_user_username %>
+  namespace_id = data.gitlab_user.project_namespace.namespace_id
 <%- endif %>
 <%- if description %>
   description = "<< description >>"
